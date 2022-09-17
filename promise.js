f8f9a332cf63c8e422aa825f0c7e6ecbbe12ced4
luthfi07 <addeluthfi0710@gmail.com>
2022-09-17 15:38:39 +0700

    selesai chalage

--- promise.js
+++ promise.js
@@ -1,7 +1,17 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) =>{
+  try {
+    const dataTeaterIXX = await promiseTheaterIXX();
+    const dataTeaterVGC = await promiseTheaterVGC();
+
+    const dataEmosi1 = dataTeaterIXX.filter((res) => res.hasil === emosi);
+    const dataEmosi2 = dataTeaterVGC.filter((res) => res.hasil === emosi);
+    
+    return dataEmosi1.length + dataEmosi2.length;
+  } catch (error) {}
+};
 
 module.exports = {
   promiseOutput,
